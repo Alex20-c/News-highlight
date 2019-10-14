07fd60895c56e7ed2405abb6de14684389cd00b8
Alex Kiprop <kipropalex59@gmail.com>
2019-10-14 10:38:59 +0300

    add a function that returns news details page and its data

--- app/views.py
+++ app/views.py
@@ -1,28 +1,32 @@
 from flask import render_template
 from app import app
-from .request import get_source, get_article
+from .request import get_source,get_article
+
 
 # Views
 @app.route('/')
 def index():
+
+
     '''
     View root page function that returns the index page and its data
     '''
 # Getting popular news
     general_news = get_source('general')
-    entertainment_news = get_source("entertainment")
-    sports_news = get_source("sports")
-
+    entertainment_news=get_source("entertainment")
+    sports_news=get_source("sports")
+   
     print(entertainment_news)
     title = 'Home - Welcome to The best News Highlight Website Online'
-    return render_template('index.html', title=title, general=general_news, entertainment=entertainment_news, sports=sports_news)
-
+    return render_template('index.html', title = title,general=general_news,entertainment=entertainment_news,sports=sports_news)
 
 @app.route('/article/<id>')
 def article(id):
+
     '''
     View news page function that returns the news details page and its data
     '''
     article = get_article(id)
     # title = f'{article.title}'
-    return render_template('article.html', article=article)
+    return render_template('article.html',article = article)
+
