8c08589850498ca10c3788b1db18273d12c07e7b
Alex Kiprop <kipropalex59@gmail.com>
2019-10-14 11:02:50 +0300

    import app from views

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,13 @@
-from app import error
-from app import views
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from .config import DevConfig
-app = Flask(__name__, instance_relative_config=True)
+app=Flask(__name__,instance_relative_config = True)
 
 app.config.from_object(DevConfig)
 app.config.from_pyfile('config.py')
 
-# Initializing Flask Exrensions
+# Initializing Flask Extensions
 bootstrap = Bootstrap(app)
+
+from app import views
+from app import error
